d1ec6713e21863187fbba62dd8475c0b18e9c730
Russell Wallace <russell.wallace@gmail.com>
2024-05-09 05:58:56 +0100

    fmt

--- Program.cs
+++ Program.cs
@@ -1,10 +1,7 @@
-﻿namespace svg_to_png
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿namespace svg_to_png {
+internal class Program {
+	static void Main(string[] args) {
+		Console.WriteLine("Hello, World!");
+	}
+}
 }
